92aeadb34ff162f543b1c61c2bf94e16aa04f1f6
Nico KraZhtest <webdev23@users.noreply.github.com>
2018-05-05 03:17:58 +0200

    Fix Firefox scroll

--- pwnyFilter.js
+++ pwnyFilter.js
@@ -33,11 +33,11 @@ hdump += '<big><bold>\
     href="javascript:void(0);" style="font-size:29px;\
     text-decoration:none;color:#111111;;vertical-align:middle;outline:0">&#9728;</a>\
   <span id="valBox">000%</span> \
-  <form style="display:inline;border:0;vertical-align:middle" onchange="pwnyFilters()">\
+  <form style="display:inline;border:0;vertical-align:middle" oninput="pwnyFilters()">\
   <input type="range" style="display:inline;border:0;outline:0;margin:0 5px 0 5px" \
     min="0" max="100" step="1" value="00" id="inVal" data-current="00"\
     oninput="if(this.value < 100){this.stepUp()};if(this.value >= 100){this.value=0}\
-    ;pwnyFilters();pwnyScroll()" /></input></form>\
+    " /></input></form>\
   <a onclick="document.body.style.cssText =\'filter:sepia(0%)invert(0%)saturate(0%)\
     brightness(100%)grayscale(0%)hue-rotate(0deg);\
     -webkit-filter:sepia(0%)invert(0%)saturate(0%)\
